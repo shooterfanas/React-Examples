b0887e7c7647b04793d616eaacf1f7352c4fcde0
shooterfanas <45656960+shooterfanas@users.noreply.github.com>
2021-09-16 23:57:28 +0300

    Dropdown-Menu

---
+++ dropdown_menu/src/components/Dropdown.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { MenuItems } from "./MenuItems";
+import { Link } from "react-router-dom";
+import "./Dropdown.css";
+
+function Dropdown() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  return (
+    <>
+      <ul
+        onClick={handleClick}
+        className={click ? "dropdown-menu clicked" : "dropdown-menu"}
+      >
+        {MenuItems.map((item, index) => {
+          return (
+            <li key={index}>
+              <Link
+                to={item.path}
+                className={item.cName}
+                onClick={() => setClick(false)}
+              >
+                {item.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default Dropdown;
---
+++ dropdown_menu/src/components/pages/Design.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Design() {
+  return (
+    <>
+      <h1 className="design">Design</h1>
+    </>
+  );
+}
