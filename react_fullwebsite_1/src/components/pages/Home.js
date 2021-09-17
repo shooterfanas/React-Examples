67cbf7e1b900ab0c56f8fa5bae9378afdc8078cb
shooterfanas <45656960+shooterfanas@users.noreply.github.com>
2021-09-17 03:02:36 +0300

    Full Website 1
    css de src de olmayan img klasörüne erişim izni olmadığı için ayrıca src içerisinde img klasörü oluşturuldu. Böylece service, product gibi sayfalarda resimler çekilebildi.

---
+++ react_fullwebsite_1/src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import "../../App.css";
+import Cards from "../Cards";
+import HeroSection from "../HeroSection";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+    </>
+  );
+}
