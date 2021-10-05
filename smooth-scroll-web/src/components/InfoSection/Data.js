78ec60b82e546de2038b8c54b1a5569a745fd168
shooterfanas <45656960+shooterfanas@users.noreply.github.com>
2021-10-05 19:50:05 +0300

    React Scroll - Single Page

---
+++ smooth-scroll-web/src/components/InfoSection/Data.js
@@ -0,0 +1,53 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Premium Bank',
+  headLine: 'Unlimited Transactions with zero fees',
+  description:
+    'Get access to our exclusive app that allows you to send unlimited transactions without getting chrged any fees.',
+  buttonLabel: 'Get Started',
+  imgStart: false,
+  img: require('../../images/svg-1.svg').default,
+  alt: 'Car',
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Unlimited Access',
+  headLine: 'Login to your account at any time',
+  description:
+    'We have you covered no matter where you are located. All you need is an internet connection and a phone or computer.',
+  buttonLabel: 'Get Started',
+  imgStart: true,
+  img: require('../../images/svg-2.svg').default,
+  alt: 'survey',
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: 'signup',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Join Our Team',
+  headLine: 'Creating an account is extremely easy',
+  description:
+    'Get everything set up and ready in under 10 minutes. All you need to do is add your information an you are ready to go.',
+  buttonLabel: 'Start Now',
+  imgStart: false,
+  img: require('../../images/svg-3.svg').default,
+  alt: 'Post',
+  dark: false,
+  primary: false,
+  darkText: true,
+};
