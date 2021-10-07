8cd597cd9efb4ec119a12c77bf96f06d2386eaa5
shooterfanas <45656960+shooterfanas@users.noreply.github.com>
2021-10-07 03:10:08 +0300

    Add files via upload

---
+++ react-modal/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Modal from './components/Modal';
+import { GlobalStyle } from './globalStyles';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Button = styled.button`
+  min-width: 100px;
+  padding: 16px 32px;
+  border-radius: 4px;
+  border: none;
+  background: #141414;
+  color: #fff;
+  font-size: 24px;
+  cursor: pointer;
+`;
+
+function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(!showModal);
+  };
+  return (
+    <Container>
+      <Button onClick={openModal}>I'm a modal</Button>
+      <Modal showModal={showModal} setShowModal={setShowModal} />
+      <GlobalStyle />
+    </Container>
+  );
+}
+
+export default App;
