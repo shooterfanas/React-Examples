de2df26d66428f80951af41bd264ff41853e07dd
shooterfanas <45656960+shooterfanas@users.noreply.github.com>
2021-10-22 03:12:32 +0300

    React Image Slider Carousel

---
+++ slider_carousel/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ImageSlider from './components/ImageSlider';
+import { SliderData } from './components/SliderData';
+
+function App() {
+  return (
+    <>
+      <ImageSlider slides={SliderData} />
+    </>
+  );
+}
+
+export default App;
---
+++ slider_carousel/src/components/SliderData.js
@@ -0,0 +1,14 @@
+export const SliderData = [
+  {
+    image: 'img/img1.jpg',
+  },
+  {
+    image: 'img/img2.jpg',
+  },
+  {
+    image: 'img/img3.jpg',
+  },
+  {
+    image: 'img/img4.jpg',
+  },
+];
