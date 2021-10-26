204767f3ed24c5e7d0a94f080e3f74ed882f6093
shooterfanas <45656960+shooterfanas@users.noreply.github.com>
2021-10-27 01:23:30 +0300

    Framer Motion

---
+++ framer-motion-pages/src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Image from '../images/img-2.jpg';
+import { motion } from 'framer-motion';
+import { animationTwo } from '../animations';
+
+const About = () => {
+  return (
+    <motion.div initial="out" animate="in" exit="out" variants={animationTwo}>
+      <Header />
+      <Hero image={Image} title="Beatiful views" desc="Never seen before" />
+    </motion.div>
+  );
+};
+
+export default About;
